c5b65ea5544bd677b4841843c42090c13eaa4d7f
Kenneth Nguyen <n.kennethcs@gmail.com>
2019-08-06 22:38:16 -0700

    Implement Book constructor and HTML form elements for user to add a new Book object to library

--- logic.js
+++ logic.js
@@ -1,11 +1,29 @@
 let myLibrary = [];
 
 // constructor
-function Book() {
+function Book(title, author, pages, read) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+}
 
+Book.prototype.info = function() {
+        return `${this.title} by ${this.author}, ${this.pages} pages, ${this.read}`;
+}
+
+function addBookToLibrary(book) {
+    myLibrary.push(book);
 }
 
 
-function addBookToLibrary() {
+var new_title = document.querySelector('#title');
+var new_author = document.querySelector('#author');
+var new_page = document.querySelector('#page');
+var new_read = document.querySelector('select');
 
-}
+const button = document.querySelector('button');
+button.addEventListener('click', function() {
+    let new_book = new Book(new_title.value, new_author.value, new_page.value, new_read.value);
+    addBookToLibrary(new_book);
+});
